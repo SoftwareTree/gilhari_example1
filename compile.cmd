f8eedea45a8b6bf52de21563b25674cceee0ce71
DamodarPeriwal <dperiwal@softwaretree.com>
2026-06-10 13:30:38 -0700

    Ensure bin directory exists before compiling

--- compile.cmd
+++ compile.cmd
@@ -1,7 +1,10 @@
 REM Compile java source files with JDK 1.8 compatibility
 REM Set JX_HOME to the root directory of Gilhari installation.
+if not exist bin mkdir bin
 REM set JX_HOME=..\..
+if not exist bin mkdir bin
 set JX_HOME=C:\Users\Damodar\workspace3\JDXNode\dev\Gilhari
+if not exist bin mkdir bin
 
 REM With JDK 1.8
 javac -d .\bin -cp .;%JX_HOME%\libs\jxclasses.jar;%JX_HOME%\external_libs\json-20240303.jar @sources.txt
